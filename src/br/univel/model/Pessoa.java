094e8c4a2208194a51ea7879a157cacc0ae98709
Lucas Medeiros <lucas_medeiros09@hotmail.com>
2016-04-05 19:30:52 -0300

    Criado a classe modelo Pessoa, com as propriedades que existem na tela gráfica.

--- src/br/univel/model/Pessoa.java
+++ src/br/univel/model/Pessoa.java
@@ -1,5 +1,8 @@
 package br.univel.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Classe modelo Pessoa
  * @author LucasM
@@ -8,4 +11,31 @@ package br.univel.model;
 
 public class Pessoa {
 
+    private IntegerProperty id;
+    private StringProperty nome;
+    private StringProperty telefone;
+    
+	public IntegerProperty getId() {
+		return id;
+	}
+	public void setId(IntegerProperty id) {
+		this.id = id;
+	}
+	public StringProperty getNome() {
+		return nome;
+	}
+	public void setNome(StringProperty nome) {
+		this.nome = nome;
+	}
+	public StringProperty getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(StringProperty telefone) {
+		this.telefone = telefone;
+	}
+   
+	public Pessoa(){
+		
+	}
+    
 }
